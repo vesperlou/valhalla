80d6c90bb9333cb6f029317c9451014ca3b80b7b
Greg Knisely <greg.knisely@mapbox.com>
2020-09-10 12:35:04 -0400

    added lanes and updated sorting.

--- src/baldr/accessrestriction.cc
+++ src/baldr/accessrestriction.cc
@@ -8,9 +8,10 @@ namespace baldr {
 AccessRestriction::AccessRestriction(const uint32_t edgeindex,
                                      const AccessType type,
                                      const uint32_t modes,
+                                     const uint32_t lanes,
                                      const uint64_t value)
-    : edgeindex_(edgeindex), type_(static_cast<uint32_t>(type)), modes_(modes), spare_(0),
-      value_(value) {
+    : edgeindex_(edgeindex), type_(static_cast<uint32_t>(type)), modes_(modes), lanes_(lanes),
+      value_(value), spare_(0) {
 }
 
 // Get the internal edge Id.
@@ -33,6 +34,11 @@ uint32_t AccessRestriction::modes() const {
   return modes_;
 }
 
+// Get the lames impacted by access restriction.
+uint32_t AccessRestriction::lanes() const {
+  return lanes_;
+}
+
 // Get the value
 uint64_t AccessRestriction::value() const {
   return value_;
@@ -47,7 +53,11 @@ void AccessRestriction::set_value(const uint64_t v) {
 bool AccessRestriction::operator<(const AccessRestriction& other) const {
   if (edgeindex() == other.edgeindex()) {
     if (modes() == other.modes()) {
-      return value() < other.value();
+      if (lanes() == other.lanes()) {
+        return value() < other.value();
+      } else {
+        return lanes() < other.lanes();
+      }
     } else {
       return modes() < other.modes();
     }
