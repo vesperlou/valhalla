0caeafd0e3e76c256665b16151b8d93bcf95f625
Pavel Merkis <pavel.merkis@mapbox.com>
2021-07-21 11:49:10 +0300

    hack

--- src/sif/autocost.cc
+++ src/sif/autocost.cc
@@ -465,6 +465,9 @@ Cost AutoCost::EdgeCost(const baldr::DirectedEdge* edge,
                         uint8_t& flow_sources) const {
   // either the computed edge speed or optional top_speed
   auto edge_speed = tile->GetSpeed(edge, flow_mask_, seconds, false, &flow_sources);
+  if (!edge->has_flow_speed() && edge->use() == Use::kTurnChannel) {
+    edge_speed /= 1.25;
+  }
   auto final_speed = std::min(edge_speed, top_speed_);
   float sec = edge->length() * speedfactor_[final_speed];
 
