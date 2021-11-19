b1116764df60affbce77588317d7ae99ed66ae50
Greg Knisely <greg.knisely@mapbox.com>
2021-11-18 22:24:09 -0500

    pronunciations and languages both now linguistics

--- valhalla/mjolnir/graphtilebuilder.h
+++ valhalla/mjolnir/graphtilebuilder.h
@@ -204,8 +204,7 @@ public:
    * @param  spd            Speed limit. [kph]
    * @param  lls            The shape of the target edge.
    * @param  names          The names of the target edge.
-   * @param  pronunciations The pronunciations of the target edge.
-   * @param  languages      The languages for the names
+   * @param  linguistics    The linguistics of the target edge.
    * @param  types          Bits indicating if the name is a ref vs a name.
    * @param  added          Set to true if the target edge was newly added to the list,
    *                        set to false if the target edge was already in the list.
@@ -224,8 +223,7 @@ public:
                        const shape_container_t& lls,
                        const std::vector<std::string>& names,
                        const std::vector<std::string>& tagged_values,
-                       const std::vector<std::string>& pronunciations,
-                       const std::vector<std::string>& languages,
+                       const std::vector<std::string>& linguistics,
                        const uint16_t types,
                        bool& added,
                        bool diff_names = false);
@@ -248,8 +246,7 @@ public:
    * @param  llstr          The shape of the target edge as an encoded string.
    * @param  names          The names of the target edge.
    * @param  tagged_values  The tagged names of the target edge.
-   * @param  pronunciations The pronunciations of the target edge.
-   * @param  languages      The languages for the names
+   * @param  linguistics    The linguistics of the target edge.
    * @param  types          Bits indicating if the name is a ref vs a name.
    * @param  added          Set to true if the target edge was newly added to the list,
    *                        set to false if the target edge was already in the list.
@@ -267,8 +264,7 @@ public:
                        const std::string& llstr,
                        const std::vector<std::string>& names,
                        const std::vector<std::string>& tagged_values,
-                       const std::vector<std::string>& pronunciations,
-                       const std::vector<std::string>& languages,
+                       const std::vector<std::string>& linguistics,
                        const uint16_t types,
                        bool& added,
                        bool diff_names = false);
