2cefa0c027c17bd9bd97b2af7478e317468379cc
Greg Knisely <greg.knisely@mapbox.com>
2021-11-30 20:24:39 -0500

    cleanup

--- valhalla/baldr/graphconstants.h
+++ valhalla/baldr/graphconstants.h
@@ -362,7 +362,7 @@ enum class TaggedValue : uint8_t { // must start at 1 due to nulls
   kLayer = 1,
   kLinguistic = 2,
   kBssInfo = 3,
-  
+
   // we used to have bug when we encoded 1 and 2 as their ASCII codes, but not actual 1 and 2 values
   // see https://github.com/valhalla/valhalla/issues/3262
   kTunnel = static_cast<uint8_t>('1'),
